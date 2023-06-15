6faea57e2e243f6e957bf049ba903cec5ff00f5f
roberto2002-03-12 <emilio.contreras@tecsup.edu.pe>
2023-06-15 10:21:34 -0500

    black token middleware

--- src/middlewares/token-valid.handler.js
+++ src/middlewares/token-valid.handler.js
@@ -1,2 +1,18 @@
 const boom = require('@hapi/boom');
-// ToDo search Token
+const { getBlackTokenByToken } = require('../services/black_token.service');
+
+function checkTokenBlack() {
+  return async (req, res, next) => {
+    const tokenBearer = req.headers.authorization;
+
+    const token = tokenBearer.split(' ');
+
+    const rta = await getBlackTokenByToken(token[1]);
+
+    if (rta) next(boom.unauthorized('Token desahabilitado'));
+
+    next();
+  };
+}
+
+module.exports = checkTokenBlack;
