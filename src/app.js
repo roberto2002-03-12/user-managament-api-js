a295c15dd5c19580f3aa331ea280cef8bf1855a0
roberto2002-03-12 <emilio.contreras@tecsup.edu.pe>
2023-06-16 15:54:52 -0500

    cors added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index');
 const {
   ormErrorHandler, errorHandler, logErrors, boomErrorHandler,
@@ -8,6 +9,7 @@ const createApp = () => {
   const app = express();
 
   app.use(express.json());
+  app.use(cors());
 
   // eslint-disable-next-line global-require
   require('./utils/auth/index');
